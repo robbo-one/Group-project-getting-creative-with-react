1e37871c18fd065a7b9da10307a032d1f4337c9b
Rob <robsc801@gmail.com>
2021-01-29 12:56:05 +1300

    robs page

--- client/components/Rob.jsx
+++ client/components/Rob.jsx
@@ -1,8 +1,35 @@
 import React from 'react'
 
+
 const Rob = () => {
+ 
+  function onClick() {
+  window.location.href="http://google.com"
+  }
+
+  function eject() {
+    window.location.href="http://google.com"
+  }
+
   return (
-  <h2>Rob</h2>
+  <div className="rob">
+
+    <h2> <a class="robTitle"
+    href="http://google.com">A L T 3000 ^</a>
+    </h2>
+
+    <button class="start"  onClick={onClick}>Engine Start</button>
+    <div><button class="eject"  onClick={eject}>EJECT</button></div>
+    
+    {/* <button> <a href='http://google.com'   type="button">Engine start</a>
+    </button> */}
+    {/* <button type="button">Click Me!</button>
+    <button type="button">Click Me!</button>
+    <button type="button">Click Me!</button>
+    <button type="button">Click Me!</button>
+    <button type="button">Click Me!</button> */}
+
+    </div>
   )
 }
 
